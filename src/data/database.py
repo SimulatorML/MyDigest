d0fcbf916604651d4ef69f8f0bc1e724401c27f7
aleksey_y <110169963+farogu@users.noreply.github.com>
2025-02-06 10:27:37 +0300

    In src/data/database.py: moved supabase client creation there from config, added imports scraper, summarize, simplified SupabaseErrorHandler exception handler, added digest creation make_digest()

--- src/data/database.py
+++ src/data/database.py
@@ -1,29 +1,29 @@
-from src.config.config import supabase
+import asyncio
+from datetime import datetime
+from typing import List, Dict, Any
+from supabase import create_client, Client
 from supabase import AuthApiError, PostgrestAPIError
+from src.config.config import SUPABASE_URL, SUPABASE_KEY
+from src.scraper import scrape_messages
+from src.summarization import summarize
+
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 class SupabaseErrorHandler:
-    """
-    A class for handling Supabase errors. It contains three methods, each of which
-    handles a specific type of error: authentication errors, API errors, and general
-    errors. Each method prints a message to the console that includes a description
-    of the error and the user ID associated with the error.
-    """
-
     @staticmethod
-    def handle_auth_error(e, user_id):
-        print(f"Ошибка аутентификации для пользователя {user_id}: {e}")
-
-    @staticmethod
-    def handle_api_error(e, user_id):
-        print(f"Ошибка API для пользователя {user_id}: {e}")
-
-    @staticmethod
-    def handle_general_error(e, user_id):
-        print(f"Произошла ошибка для пользователя {user_id}: {e}")
+    def handle_error(e: Exception, user_id: int) -> None:
+        error_map = {
+            AuthApiError: ("AuthError", 500),
+            PostgrestAPIError: ("ApiError", 400),
+            ConnectionError: ("ConnectionError", 503),
+        }
+        msg = f"{error_map.get(type(e))[0]} у пользователя {user_id}: {str(e)}"
+        print(msg)  # В будущем заменить на логирование
 
 
-async def fetch_user(user_id):
+async def fetch_user(user_id: int) -> int:
     """
     Get a user from the database by their ID.
 
@@ -32,20 +32,19 @@ async def fetch_user(user_id):
              fetched.
     """
     try:
-        response = supabase.table("users").select("user_id").eq("user_id", user_id).execute()
-        return response.data[0]["user_id"] if response.data else None    # Return only user_id value
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-        return None
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
-        return None
+        response = (
+            supabase.table("users").select("user_id").eq("user_id", user_id).execute()
+        )
+        return (
+            response.data[0]["user_id"] if response.data else None
+        )  # Return only user_id value
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
-        return None
+        SupabaseErrorHandler.handle_error(e, user_id)
 
 
-async def add_user(user_id, username, login_timestamp=None):
+async def add_user(
+    user_id: int, username: str, login_timestamp: str = None
+) -> Dict[str, Any]:
     """
     Add a user to the database.
 
@@ -72,18 +71,11 @@ async def add_user(user_id, username, login_timestamp=None):
             print(f"Ошибка при добавлении пользователя: {response.data}")
 
         return response.data
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-        return []
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
-        return []
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
-        return []
+        SupabaseErrorHandler.handle_error(e, user_id)
 
 
-async def fetch_user_channels(user_id):
+async def fetch_user_channels(user_id: int) -> Dict[str, Any]:
     """
     Get a list of all channels for a given user_id from the database.
 
@@ -94,24 +86,16 @@ async def fetch_user_channels(user_id):
     """
     try:
         response = (
-            supabase.table("user_channels")
-            .select("channel_id, channel_name")
-            .eq("user_id", user_id)
-            .execute()
+            supabase.table("user_channels").select("*").eq("user_id", user_id).execute()
         )
-        return response.data if response.data else None  # Return channel_id and channel_name
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-        return []
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
-        return []
+        return response.data if response.data else None
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
-        return []
+        SupabaseErrorHandler.handle_error(e, user_id)
 
 
-async def add_user_channels(user_id, channels, addition_timestamp=None):
+async def add_user_channels(
+    user_id: int, channels: List[str], addition_timestamp: str = None
+) -> bool:
     """
     Add channels for a given user to the database.
 
@@ -138,16 +122,11 @@ async def add_user_channels(user_id, channels, addition_timestamp=None):
         else:
             print(f"Ошибка при записи данных: {response.data}")
             return False
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
-    return False
+        SupabaseErrorHandler.handle_error(e, user_id)
 
 
-async def delete_user_channels(user_id, channels):
+async def delete_user_channels(user_id: int, channels: List[str]) -> bool:
     """
     Delete specified channels associated with a given user from the database.
 
@@ -161,15 +140,11 @@ async def delete_user_channels(user_id, channels):
                 "channel_name", channel
             ).execute()
         return True
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
+        SupabaseErrorHandler.handle_error(e, user_id)
 
 
-async def clear_user_channels(user_id):
+async def clear_user_channels(user_id: int) -> bool:
     """
     Clear all channels associated with a given user from the database.
 
@@ -179,17 +154,62 @@ async def clear_user_channels(user_id):
     try:
         supabase.table("user_channels").delete().eq("user_id", user_id).execute()
         return True
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
+        SupabaseErrorHandler.handle_error(e, user_id)
+
+
+async def make_digest(user_id: int, time_range: str = "24h") -> None:
+    try:
+        exist_user = await fetch_user(user_id)
+        if not exist_user:
+            print(f"Пользователь {user_id} НЕ существует в БД")
+            return
+
+        user_channels = await fetch_user_channels(user_id) if exist_user else None
+
+        for channel in user_channels:
+            try:
+                messages = await scrape_messages(
+                    channel["channel_name"], limit=5, time_range=time_range
+                )  # scrape a limited numbers of messages from each channel over the last 24 hours
+                if not messages:
+                    print(f"Нет сообщений для канала {channel['channel_name']}.")
+                    continue
+                # if messages:
+                digest_content = summarize(messages, channel["channel_name"])
+                creation_timestamp = datetime.now().isoformat()
+                await save_user_digest(
+                    user_id, channel["channel_id"], digest_content, creation_timestamp
+                )
+
+            except Exception as e:
+                print(f"Ошибка при создании дайджеста для пользователя {user_id}: {e}")
+
+    except Exception as e:
+        print(f"Ошибка при обработке канала {channel['channel_name']}: {e}")
+
+    finally:
+        # Даем время на завершение фоновых задач - тогда RuntimeWarning возникает реже
+        await asyncio.sleep(1)
 
 
 async def save_user_digest(
     user_id: int, channel_id: int, digest_content: str, creation_timestamp: str = None
-):
+) -> bool:
+    """
+    Save a user's digest to the database.
+
+    This function attempts to upsert a digest record for a given user and channel
+    into the "digests" table. If the operation is successful, the function returns
+    True. If the operation fails, it returns False and logs an error message.
+
+    :param user_id: The ID of the user for whom the digest is being saved.
+    :param channel_id: The ID of the channel associated with the digest.
+    :param digest_content: The content of the digest to be saved.
+    :param creation_timestamp: The timestamp of when the digest was created. Defaults to None.
+    :return: True if the digest is successfully saved, False otherwise.
+    """
+
     try:
         response = (
             supabase.table("digests")
@@ -209,27 +229,21 @@ async def save_user_digest(
         else:
             print(f"Ошибка при записи дайджеста: {response.data}")
             return False
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
-    return False
+        SupabaseErrorHandler.handle_error(e, user_id)
 
 
-async def fetch_user_digests(user_id):
+async def fetch_user_digests(user_id: int) -> List[str]:
+    """
+    Fetches all digests for a given user from the database.
+
+    :param user_id: The ID of the user for whom to fetch digests.
+    :return: A list of strings where each string is the content of a digest for the user.
+    """
     try:
         response = (
             supabase.table("digests").select("content").eq("user_id", user_id).execute()
         )
         return [item["content"] for item in response.data] if response.data else None
-    except AuthApiError as e:
-        SupabaseErrorHandler.handle_auth_error(e, user_id)
-        return []
-    except PostgrestAPIError as e:
-        SupabaseErrorHandler.handle_api_error(e, user_id)
-        return []
     except Exception as e:
-        SupabaseErrorHandler.handle_general_error(e, user_id)
-        return []
+        SupabaseErrorHandler.handle_error(e, user_id)
