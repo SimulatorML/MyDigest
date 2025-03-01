a73b9a86ec21f03746d499ff0b29d8342a8d1682
aleksey_y <110169963+farogu@users.noreply.github.com>
2025-03-01 10:49:00 +0300

    Added the 'is_active' parameter to the user_channels table, clarified the data type for int (int8 <-> bigint)

--- src/data/database.sql
+++ src/data/database.sql
@@ -1,31 +1,32 @@
 -- users table
 CREATE TABLE IF NOT EXISTS users (
-    user_id int8 NOT NULL PRIMARY KEY,
+    user_id bigint NOT NULL PRIMARY KEY,
     username varchar(255) NOT NULL,
     login_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP WITHOUT TIME ZONE
 );
 
 --  users_channels table
 CREATE TABLE IF NOT EXISTS user_channels (
-    channel_id int8 NOT NULL PRIMARY KEY,
-    user_id int8 NOT NULL REFERENCES users(user_id),
+    channel_id bigint NOT NULL PRIMARY KEY,
+    user_id bigint NOT NULL REFERENCES users(user_id),
     channel_name varchar(255) NOT NULL,
     channel_link varchar(255) NULL,
-    addition_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP WITHOUT TIME ZONE
+    addition_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP WITHOUT TIME ZONE,
+    is_active boolean NOT NULL default true
 );
 
 -- channels_news table
 CREATE TABLE IF NOT EXISTS channels_news (
-    news_id int8 NOT NULL PRIMARY KEY,
-    channel_id int8 NOT NULL REFERENCES user_channels(channel_id),
+    news_id bigint NOT NULL PRIMARY KEY,
+    channel_id bigint NOT NULL REFERENCES user_channels(channel_id),
     news varchar(255) NOT NULL,
     addition_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP WITHOUT TIME ZONE
 );
 
 -- digests table
 CREATE TABLE IF NOT EXISTS digests (
-    digest_id int8 NOT NULL PRIMARY KEY,
-    user_id int8 NOT NULL REFERENCES users(user_id),
+    digest_id bigint NOT NULL PRIMARY KEY,
+    user_id bigint NOT NULL REFERENCES users(user_id),
     digest_content varchar(255) NOT NULL,
     creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP WITHOUT TIME ZONE
 );
