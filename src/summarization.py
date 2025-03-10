3bfc6258a7e15d8afe4a6109f8c274037f194914
DinaYatsuk <d.yatsuk97@gmail.com>
2025-03-10 18:17:50 +0700

    enhanced prompt

--- src/summarization.py
+++ src/summarization.py
@@ -58,8 +58,9 @@ class Summarization:
             return "No items available for clustering."
 
         prompt = (
-            f''' Please categorize the following news summaries into a maximum of 5 broad, topic-based clusters. Each cluster should be grouped by similar topics, and the summaries should remain in their original format with each followed by its relevant link(s).
-                Each topics must be closed into HTML tags highlighting them bold: <b>Topic</b>. Don't use ** to highlight topics with bold!
+            f''' Please categorize the following news summaries into a maximum of 5 broad, topic-based clusters.
+                Each cluster should be grouped by similar topics, and the summaries should remain in their original format with each followed by its relevant link(s). 
+                Each topics must be closed into HTML tags highlighting them bold: <b>Topic</b>. Don't use ** to highlight topics with bold! Never add ```html ```!
                 The topic labels must be written in Russian, and each topic must be followed by a new line.
                 Each topic should be introduced with a one relevant emoji. Emoji should only be placed in front of topic.
                 Ensure the topics are broad and general; limit the number of topics to 5.
