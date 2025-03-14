d744c0f350de82e3258b9b0f925c718a2a32dff7
Dmitry Polikarpov <polikarpov5567@gmail.com>
2025-03-14 14:25:47 +0500

    async with httpx.AsyncClient() + env.sample upds

--- src/utils/telegram_logger.py
+++ src/utils/telegram_logger.py
@@ -1,8 +1,7 @@
-import requests
+import httpx
 import logging
 from datetime import datetime
 from src.config import GROUP_LOGS_ID, TELEGRAM_BOT_TOKEN, TURN_TG_LOGGER, TITLE_TG_LOGGER
-
 
 class TelegramSender:
     def __init__(self, token=TELEGRAM_BOT_TOKEN, turn=TURN_TG_LOGGER, title=TITLE_TG_LOGGER):
@@ -14,23 +13,24 @@ class TelegramSender:
     async def send_text(self, text: str, channel_id=GROUP_LOGS_ID):
         if not self.turn:
             return
-        
+
         # Если логгер включен, то
         method = f"{self.url}/sendMessage"
         logging.info(f'Sending message to {channel_id}: {text}')
-        
+
         # Формируем текст: заголовок + текст + дата
         full_text = (
             f"{self.title}\n"  # Заголовок для сообщений, например, Testing
             f"{text}\n\n"      # Основной текст
             f"{datetime.now()}"  # Время отправки
         )
-        
-        r = requests.post(method, data={
+
+        async with httpx.AsyncClient() as client:
+            r = await client.post(method, data={
             "chat_id": channel_id,
             "text": full_text
         })
-        
+
         if r.status_code != 200:
             raise Exception(f"post_text error: {r.status_code}, {r.text}")
 
