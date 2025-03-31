63c0ecd03d88f3b5a6b435bcfab9526fdfb3564e
aleksey_y <110169963+farogu@users.noreply.github.com>
2025-03-31 11:17:14 +0300

    Fixed whitespaces in stop_auto_news_check()

--- src/scraper.py
+++ src/scraper.py
@@ -197,7 +197,7 @@ class TelegramScraper:
                                             parse_mode="HTML",
                                             disable_web_page_preview=True)
         except Exception as e:
-            logging.error("Ошибка в check_new_messages: %s", e)
+            logging.error("\nОшибка в check_new_messages для пользователя %s: %s\n", user_id, e)
             await self.bot.send_message(user_id, "❌ Ошибка при получении дайджеста. Попробуйте позже.")
 
     async def start_auto_news_check(self, user_id: int, interval: int = 1800):
@@ -226,21 +226,21 @@ class TelegramScraper:
 
     @staticmethod
     def stop_auto_news_check(user_id: int):
-            """
-            Stop the background task checking for new messages for the specified user.
+        """
+        Stop the background task checking for new messages for the specified user.
 
-            This method cancels the background task associated with the user, effectively stopping
-            further periodic message checks and digest updates.
+        This method cancels the background task associated with the user, effectively stopping
+        further periodic message checks and digest updates.
 
-            :param user_id: The unique identifier of the user.
-            :return: True if the background task was successfully stopped, otherwise False.
-            :raises: Exception if stopping the task fails.
-            """
-            if user_id in TelegramScraper.running_tasks:
-                TelegramScraper.running_tasks[user_id].cancel()
-                del TelegramScraper.running_tasks[user_id]
-                return True
-            return False
+        :param user_id: The unique identifier of the user.
+        :return: True if the background task was successfully stopped, otherwise False.
+        :raises: Exception if stopping the task fails.
+        """
+        if user_id in TelegramScraper.running_tasks:
+            TelegramScraper.running_tasks[user_id].cancel()
+            del TelegramScraper.running_tasks[user_id]
+            return True
+        return False
 
     async def scrape_messages_long_term(self, entity_name: str, days: int = 2, limit: int = 1000) -> List[Dict[str, Union[int, str, datetime]]]:
         """
