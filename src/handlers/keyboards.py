5fb5544bb09992368e1ac9037dfb92b08b11527e
Dmitry Polikarpov <polikarpov5567@gmail.com>
2025-04-01 00:28:47 +0500

    upd handle_interval_btn and in keyboards

--- src/handlers/keyboards.py
+++ src/handlers/keyboards.py
@@ -1,8 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 #keyboard for bot's menu
-menu = ReplyKeyboardMarkup(keyboard=
-    [KeyboardButton(text='⭐️ Получать новости'), [KeyboardButton(text='⏲️ Установить интервал')],
+menu = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='⭐️ Получать новости'), KeyboardButton(text='⏲️ Установить интервал')],
     [KeyboardButton(text='🛑 Остановить новости'), KeyboardButton(text='🗑 Удалить каналы')]
 ],
                     resize_keyboard=True,
